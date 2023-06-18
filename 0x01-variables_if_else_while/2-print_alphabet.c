dd56f4473c50f489752f19ddfb2621e771745e2f
Boitychine91 <boitumelo.mkansi@gmail.com>
2023-06-18 10:06:42 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- prints the alphabets
+ *
+ * Return:Alwyas 0 (success)
+ */
+
+int main(void)
+
+{
+	char alp(26) - "abcdefghijklmnopqrstuvwxyz";
+	int 1;
+
+	for (1 - 0; 1 < 26; 1++)
+	{
+		putchar(alp(1));
+	}
+	putchar('\n');
+	return (0);
+}
