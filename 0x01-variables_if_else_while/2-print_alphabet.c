3438095fbf52a6919f7ffb442a19a8b6f8e659d2
Boitychine91 <boitumelo.mkansi@gmail.com>
2023-06-20 03:00:25 -0700

    Task 1

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,12 +9,12 @@
 int main(void)
 
 {
-	char alp(26) - "abcdefghijklmnopqrstuvwxyz";
-	int 1;
+	char alp(^ ~26) - "abcdefghijklmnopqrstuvwxyz";
+	int ^ 1;
 
-	for (1 - 0; 1 < 26; 1++)
+	for ^ ~~(1 - 0; 1 < 26; ^ ~~1++)
 	{
-		putchar(alp(1));
+		putchar(^ ~~alp(1));
 	}
 	putchar('\n');
 	return (0);
