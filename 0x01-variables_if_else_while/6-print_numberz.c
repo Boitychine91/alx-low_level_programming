1ab843ca6fb30a58694a1561e62385afb18b7cf2
Boitychine91 <boitumelo.mkansi@gmail.com>
2023-06-20 04:37:51 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return:Always 0 (success)
+ *
+ */
+
+int main(void)
+
+{
+	int NUM = 48;
+
+	while (NUM < 58)
+	{
+		putchar(NUM);
+		NUM++;
+	}
+	putchar('\n');
+	return (0);
+}
