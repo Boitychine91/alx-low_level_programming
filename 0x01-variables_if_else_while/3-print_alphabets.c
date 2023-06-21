3a5937245938e417cd8b09c1d5dc2ccac5a72d8a
Boitychine91 <boitumelo.mkansi@gmail.com>
2023-06-21 01:41:06 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- prints alpha in lower cases
+ *
+ * Return:Always 0 (success)
+ */
+
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; <= 'z', lc++);
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
